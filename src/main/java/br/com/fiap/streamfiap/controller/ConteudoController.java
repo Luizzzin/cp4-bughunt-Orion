c6cb8ae7d76d1da5a550743c9c3b0f4d31ec2a40
Kenshin <kenshin.matayoshi@gmail.com>
2026-09-10 18:39:52 -0300

    refactor: Clean code4 - Encaplusar duracaoMinutos no ConteudoController

--- src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
+++ src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -51,11 +50,11 @@ public class ConteudoController {
     // POST /api/conteudos/filme - cadastra um filme (cria nova instância sem o id vindo do cliente)
     @PostMapping("/filme")
     public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme) {
-        if (filme.duracaoMinutos <= 0) {
+        if (filme.getDuracaoMinutos() <= 0) {
             throw new IllegalArgumentException("A duração do filme deve ser maior que zero minutos!");
         }
 
-        Filme novo = new Filme(filme.getTitulo(), filme.getCategoria(), filme.duracaoMinutos,
+        Filme novo = new Filme(filme.getTitulo(), filme.getCategoria(), filme.getDuracaoMinutos(),
                 filme.getClassificacaoEtaria(), filme.isDisponivel(), filme.isEstreia());
         return ResponseEntity.status(201).body(conteudoRepository.save(novo));
     }
@@ -63,10 +62,10 @@ public class ConteudoController {
     // POST /api/conteudos/serie - cadastra uma série
     @PostMapping("/serie")
     public ResponseEntity<Serie> cadastrarSerie(@RequestBody Serie serie) {
-        if (serie.duracaoMinutos <= 0) {
+        if (serie.getDuracaoMinutos() <= 0) {
             throw new IllegalArgumentException("A duração da serie deve ser maior que zero minutos!");
         }
-        Serie nova = new Serie(serie.getTitulo(), serie.getCategoria(), serie.duracaoMinutos,
+        Serie nova = new Serie(serie.getTitulo(), serie.getCategoria(), serie.getDuracaoMinutos(),
                 serie.getClassificacaoEtaria(), serie.getNumeroTemporadas());
         return ResponseEntity.status(201).body(conteudoRepository.save(nova));
     }
@@ -78,7 +77,7 @@ public class ConteudoController {
             throw new IllegalArgumentException("A duração do documentario deve ser maior que zero minutos!");
         }
         Documentario novo = new Documentario(documentario.getTitulo(), documentario.getCategoria(),
-                documentario.duracaoMinutos, documentario.getClassificacaoEtaria(),
+                documentario.getDuracaoMinutos(), documentario.getClassificacaoEtaria(),
                 documentario.isDisponivel(), documentario.getTema());
         return ResponseEntity.status(201).body(conteudoRepository.save(novo));
     }
